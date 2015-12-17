d81b8ae95780ae9a66a9e10061312ef37d72b367
Lakshay Bhambri <bhambri.lakshay.lakshay@gmail.com>
2015-12-17 12:37:01 +0530

    updated INPUT.php (json support added)

--- classes/core/INPUT.php
+++ classes/core/INPUT.php
@@ -1,28 +1,22 @@
 <?php 
-class INPUT{
-	
+class Input{
     public static function exists($type = 'post'){
         switch($type){
             case 'post' : 
                 return (!empty($_POST)) ? true : false;
             case 'get' :
                 return (!empty($_GET)) ? true : false;
+            case 'json' :
+                return (!empty(json_decode(file_get_contents('php://input'), true))) ? true : false;
             default :
                 return false;
         }
     }
     
-    public static function getToken(){
-        if(!SESSION::exists('csrf_token')){
-           SESSION::setSession("csrf_token",HASH::generateRandomString(20));
-        }
-        return SESSION::getSession("csrf_token"); 
-    }
-    
     public static function get($item){
         if(isset($_POST[$item])){
             if(is_array($_POST[$item])){
-                for($i=0; $i<count($_POST[$item]); $i++){
+                for($i=0; $i<count($_POST[$item]); $++){
                     $_POST[$item][i] = self::sanitizeInput($_POST[$item][i]); 
                 }
                 return $_POST[$item];
@@ -32,7 +26,7 @@ class INPUT{
         }
         else if(isset($_GET[$item])){
             if(is_array($_GET[$item])){
-                for($i=0; $i<count($_GET[$item]); $i++){
+                for($i=0; $i<count($_GET[$item]); $++){
                     $_GET[$item][i] = self::sanitizeInput($_GET[$item][i]); 
                 }
                 return $_GET[$item];
@@ -40,16 +34,28 @@ class INPUT{
             else
                 return self::sanitizeInput($_GET[$item]);
         }
-        return '';
+        
+        else {
+            $data = json_decode(file_get_contents('php://input'), true);
+            if(!empty($data)){
+                if(is_array($data[$item])){
+                    for($i=0; $i<count($data[$item]); $++){
+                        $data[$item][i] = self::sanitizeInput($data[$item][i]);
+                    }
+                    return $data[$item];
+                }
+                else
+                    return self::sanitizeInput($data[$item]);
+            }
+        }
+        return throw new CustomException("$item is not a request parameter.");
     }
     
-    public static function isValidRequest(){
-        if(INPUT::get('csrf_token') && SESSION::exists('csrf_token')){    //or access token_name from config file.
-            if(Input::get('csrf_token') == SESSION::getSession('csrf_token'))
-                SESSION::deleteSession ("csrf_token");
+    public static function isValid(){
+        if(self::exists('csrf_token') && Session::exists('csrf_token')){    //or access token_name from config file.
+            if(Input::get('csrf_token') == Session::get('csrf_token'))
                 return true;
         }
-        SESSION::deleteSession("csrf_token"); 
         return false;
     }
     
@@ -62,21 +68,3 @@ class INPUT{
         return $input;
     }
 }
-
-/*
-
-
-//use trim() before applying this shit
-FILTER_SANITIZE_NUMBER_INT  =>  for numbers only
-FILTER_VALIDATE_EMAIL   =>  for email
-
-FILTER_SANITIZE_STRING  =>  mother of all filters
-//FILTER_SANITIZE_URL     =>url               /*DONT USE THIS - THIS MOTHERFUCKER NOT USEFUL*/
-
-
-/*
-
-IDEA :=     Using sanitizing in validation if validation is successful?    - BTa HIMACHU and LAkSHAY BRO
-*/
-
-
